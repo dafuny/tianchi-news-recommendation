b71c204111659d05c83c8b4e7256b4b06a9e679c
furong <48078703+dafuny@users.noreply.github.com>
2024-12-09 23:35:10 +0800

    Update rank_feature.py

--- code/rank_feature.py
+++ code/rank_feature.py
@@ -36,7 +36,7 @@ log.info(f'排序特征，mode: {mode}')
 
 def func_if_sum(x):
     user_id = x['user_id']
-    article_id = x['article_id']
+    mrch_id = x['mrch_id']
 
     interacted_items = user_item_dict[user_id]
     interacted_items = interacted_items[::-1]
@@ -44,7 +44,7 @@ def func_if_sum(x):
     sim_sum = 0
     for loc, i in enumerate(interacted_items):
         try:
-            sim_sum += item_sim[i][article_id] * (0.7**loc)
+            sim_sum += item_sim[i][mrch_id] * (0.7**loc)
         except Exception as e:
             pass
     return sim_sum
@@ -52,13 +52,13 @@ def func_if_sum(x):
 
 def func_if_last(x):
     user_id = x['user_id']
-    article_id = x['article_id']
+    mrch_id = x['mrch_id']
 
     last_item = user_item_dict[user_id][-1]
 
     sim = 0
     try:
-        sim = item_sim[last_item][article_id]
+        sim = item_sim[last_item][mrch_id]
     except Exception as e:
         pass
     return sim
@@ -66,13 +66,13 @@ def func_if_last(x):
 
 def func_binetwork_sim_last(x):
     user_id = x['user_id']
-    article_id = x['article_id']
+    mrch_id = x['mrch_id']
 
     last_item = user_item_dict[user_id][-1]
 
     sim = 0
     try:
-        sim = binetwork_sim[last_item][article_id]
+        sim = binetwork_sim[last_item][mrch_id]
     except Exception as e:
         pass
     return sim
@@ -88,7 +88,7 @@ def consine_distance(vector1, vector2):
 
 def func_w2w_sum(x, num):
     user_id = x['user_id']
-    article_id = x['article_id']
+    mrch_id = x['mrch_id']
 
     interacted_items = user_item_dict[user_id]
     interacted_items = interacted_items[::-1][:num]
@@ -96,8 +96,8 @@ def func_w2w_sum(x, num):
     sim_sum = 0
     for loc, i in enumerate(interacted_items):
         try:
-            sim_sum += consine_distance(article_vec_map[article_id],
-                                        article_vec_map[i])
+            sim_sum += consine_distance(mrch_vec_map[mrch_id],
+                                        mrch_vec_map[i])
         except Exception as e:
             pass
     return sim_sum
@@ -105,14 +105,14 @@ def func_w2w_sum(x, num):
 
 def func_w2w_last_sim(x):
     user_id = x['user_id']
-    article_id = x['article_id']
+    mrch_id = x['mrch_id']
 
     last_item = user_item_dict[user_id][-1]
 
     sim = 0
     try:
-        sim = consine_distance(article_vec_map[article_id],
-                               article_vec_map[last_item])
+        sim = consine_distance(mrch_vec_map[mrch_id],
+                               mrch_vec_map[last_item])
     except Exception as e:
         pass
     return sim
@@ -127,48 +127,26 @@ if __name__ == '__main__':
         df_feature = pd.read_pickle('../user_data/data/online/recall.pkl')
         df_click = pd.read_pickle('../user_data/data/online/click.pkl')
 
-    # 文章特征
+    # 商户特征
     log.debug(f'df_feature.shape: {df_feature.shape}')
 
-    df_article = pd.read_csv('../tcdata/articles.csv')
-    df_article['created_at_ts'] = df_article['created_at_ts'] / 1000
-    df_article['created_at_ts'] = df_article['created_at_ts'].astype('int')
-    df_feature = df_feature.merge(df_article, how='left')
-    df_feature['created_at_datetime'] = pd.to_datetime(
-        df_feature['created_at_ts'], unit='s')
-
-    log.debug(f'df_article.head(): {df_article.head()}')
-    log.debug(f'df_feature.shape: {df_feature.shape}')
-    log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
-
+    df_mrch = pd.read_csv('../tcdata/marchant.csv')
+    
     # 历史记录相关特征
-    df_click.sort_values(['user_id', 'click_timestamp'], inplace=True)
-    df_click.rename(columns={'click_article_id': 'article_id'}, inplace=True)
-    df_click = df_click.merge(df_article, how='left')
+    df_click.sort_values(['user_id', 'action_time'], inplace=True)
+    df_click.rename(columns={'mrch_id': 'mrch_id'}, inplace=True)
+    df_click = df_click.merge(df_mrch, how='left')
 
-    df_click['click_timestamp'] = df_click['click_timestamp'] / 1000
-    df_click['click_datetime'] = pd.to_datetime(df_click['click_timestamp'],
+    # df_click['action_time'] = df_click['action_time'] / 1000
+    df_click['action_time'] = pd.to_datetime(df_click['action_time'],
                                                 unit='s',
                                                 errors='coerce')
-    df_click['click_datetime_hour'] = df_click['click_datetime'].dt.hour
+    # df_click['click_datetime_hour'] = df_click['click_datetime'].dt.hour
 
-    # 用户点击文章的创建时间差的平均值
-    df_click['user_id_click_article_created_at_ts_diff'] = df_click.groupby(
-        ['user_id'])['created_at_ts'].diff()
-    df_temp = df_click.groupby([
-        'user_id'
-    ])['user_id_click_article_created_at_ts_diff'].mean().reset_index()
-    df_temp.columns = [
-        'user_id', 'user_id_click_article_created_at_ts_diff_mean'
-    ]
-    df_feature = df_feature.merge(df_temp, how='left')
-
-    log.debug(f'df_feature.shape: {df_feature.shape}')
-    log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
-
-    # 用户点击文章的时间差的平均值
+    
+    # 用户点击商户的时间差的平均值
     df_click['user_id_click_diff'] = df_click.groupby(
-        ['user_id'])['click_timestamp'].diff()
+        ['user_id'])['action_time'].diff()
     df_temp = df_click.groupby(['user_id'
                                 ])['user_id_click_diff'].mean().reset_index()
     df_temp.columns = ['user_id', 'user_id_click_diff_mean']
@@ -177,37 +155,12 @@ if __name__ == '__main__':
     log.debug(f'df_feature.shape: {df_feature.shape}')
     log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
 
-    df_click['click_timestamp_created_at_ts_diff'] = df_click[
-        'click_timestamp'] - df_click['created_at_ts']
 
-    # 点击文章的创建时间差的统计值
-    df_temp = df_click.groupby(
-        ['user_id'])['click_timestamp_created_at_ts_diff'].agg({
-            'user_click_timestamp_created_at_ts_diff_mean':
-            'mean',
-            'user_click_timestamp_created_at_ts_diff_std':
-            'std'
-        }).reset_index()
-    df_feature = df_feature.merge(df_temp, how='left')
-
-    log.debug(f'df_feature.shape: {df_feature.shape}')
-    log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
-
-    # 点击的新闻的 click_datetime_hour 统计值
-    df_temp = df_click.groupby(['user_id'])['click_datetime_hour'].agg({
-        'user_click_datetime_hour_std':
-        'std'
-    }).reset_index()
-    df_feature = df_feature.merge(df_temp, how='left')
-
-    log.debug(f'df_feature.shape: {df_feature.shape}')
-    log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
-
-    # 点击的新闻的 words_count 统计值
+    # 点击的商户的 brand_nm 统计值
     df_temp = df_click.groupby(['user_id'])['words_count'].agg({
-        'user_clicked_article_words_count_mean':
+        'user_clicked_mrch_words_count_mean':
         'mean',
-        'user_click_last_article_words_count':
+        'user_click_last_mrch_words_count':
         lambda x: x.iloc[-1]
     }).reset_index()
     df_feature = df_feature.merge(df_temp, how='left')
@@ -215,23 +168,12 @@ if __name__ == '__main__':
     log.debug(f'df_feature.shape: {df_feature.shape}')
     log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
 
-    # 点击的新闻的 created_at_ts 统计值
-    df_temp = df_click.groupby('user_id')['created_at_ts'].agg({
-        'user_click_last_article_created_time':
+    
+    # 点击的商户的 action_time 统计值
+    df_temp = df_click.groupby('user_id')['action_time'].agg({
+        'user_click_last_mrch_click_time':
         lambda x: x.iloc[-1],
-        'user_clicked_article_created_time_max':
-        'max',
-    }).reset_index()
-    df_feature = df_feature.merge(df_temp, how='left')
-
-    log.debug(f'df_feature.shape: {df_feature.shape}')
-    log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
-
-    # 点击的新闻的 click_timestamp 统计值
-    df_temp = df_click.groupby('user_id')['click_timestamp'].agg({
-        'user_click_last_article_click_time':
-        lambda x: x.iloc[-1],
-        'user_clicked_article_click_time_mean':
+        'user_clicked_mrch_click_time_mean':
         'mean',
     }).reset_index()
     df_feature = df_feature.merge(df_temp, how='left')
@@ -240,17 +182,17 @@ if __name__ == '__main__':
     log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
 
     df_feature['user_last_click_created_at_ts_diff'] = df_feature[
-        'created_at_ts'] - df_feature['user_click_last_article_created_time']
-    df_feature['user_last_click_timestamp_diff'] = df_feature[
-        'created_at_ts'] - df_feature['user_click_last_article_click_time']
+        'created_at_ts'] - df_feature['user_click_last_mrch_created_time']
+    df_feature['user_last_action_time_diff'] = df_feature[
+        'created_at_ts'] - df_feature['user_click_last_mrch_click_time']
     df_feature['user_last_click_words_count_diff'] = df_feature[
-        'words_count'] - df_feature['user_click_last_article_words_count']
+        'words_count'] - df_feature['user_click_last_mrch_words_count']
 
     log.debug(f'df_feature.shape: {df_feature.shape}')
     log.debug(f'df_feature.columns: {df_feature.columns.tolist()}')
 
     # 计数统计
-    for f in [['user_id'], ['article_id'], ['user_id', 'category_id']]:
+    for f in [['user_id'], ['mrch_id'], ['user_id', 'brand_nm']]:
         df_temp = df_click.groupby(f).size().reset_index()
         df_temp.columns = f + ['{}_cnt'.format('_'.join(f))]
 
@@ -261,9 +203,9 @@ if __name__ == '__main__':
 
     # 召回相关特征
     ## itemcf 相关
-    user_item_ = df_click.groupby('user_id')['article_id'].agg(
+    user_item_ = df_click.groupby('user_id')['mrch_id'].agg(
         list).reset_index()
-    user_item_dict = dict(zip(user_item_['user_id'], user_item_['article_id']))
+    user_item_dict = dict(zip(user_item_['user_id'], user_item_['mrch_id']))
 
     if mode == 'valid':
         f = open('../user_data/sim/offline/itemcf_sim.pkl', 'rb')
@@ -275,11 +217,11 @@ if __name__ == '__main__':
         f.close()
 
     # 用户历史点击物品与待预测物品相似度
-    df_feature['user_clicked_article_itemcf_sim_sum'] = df_feature[[
-        'user_id', 'article_id'
+    df_feature['user_clicked_mrch_itemcf_sim_sum'] = df_feature[[
+        'user_id', 'mrch_id'
     ]].parallel_apply(func_if_sum, axis=1)
-    df_feature['user_last_click_article_itemcf_sim'] = df_feature[[
-        'user_id', 'article_id'
+    df_feature['user_last_click_mrch_itemcf_sim'] = df_feature[[
+        'user_id', 'mrch_id'
     ]].parallel_apply(func_if_last, axis=1)
 
     log.debug(f'df_feature.shape: {df_feature.shape}')
@@ -295,8 +237,8 @@ if __name__ == '__main__':
         binetwork_sim = pickle.load(f)
         f.close()
 
-    df_feature['user_last_click_article_binetwork_sim'] = df_feature[[
-        'user_id', 'article_id'
+    df_feature['user_last_click_mrch_binetwork_sim'] = df_feature[[
+        'user_id', 'mrch_id'
     ]].parallel_apply(func_binetwork_sim_last, axis=1)
 
     log.debug(f'df_feature.shape: {df_feature.shape}')
@@ -304,19 +246,19 @@ if __name__ == '__main__':
 
     ## w2v 相关
     if mode == 'valid':
-        f = open('../user_data/data/offline/article_w2v.pkl', 'rb')
-        article_vec_map = pickle.load(f)
+        f = open('../user_data/data/offline/mrch_w2v.pkl', 'rb')
+        mrch_vec_map = pickle.load(f)
         f.close()
     else:
-        f = open('../user_data/data/online/article_w2v.pkl', 'rb')
-        article_vec_map = pickle.load(f)
+        f = open('../user_data/data/online/mrch_w2v.pkl', 'rb')
+        mrch_vec_map = pickle.load(f)
         f.close()
 
-    df_feature['user_last_click_article_w2v_sim'] = df_feature[[
-        'user_id', 'article_id'
+    df_feature['user_last_click_mrch_w2v_sim'] = df_feature[[
+        'user_id', 'mrch_id'
     ]].parallel_apply(func_w2w_last_sim, axis=1)
-    df_feature['user_click_article_w2w_sim_sum_2'] = df_feature[[
-        'user_id', 'article_id'
+    df_feature['user_click_mrch_w2w_sim_sum_2'] = df_feature[[
+        'user_id', 'mrch_id'
     ]].parallel_apply(lambda x: func_w2w_sum(x, 2), axis=1)
 
     log.debug(f'df_feature.shape: {df_feature.shape}')
